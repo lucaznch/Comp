36e13c15e201be414df63df68a6baab5382255ca
Gabriel Silva <gabriel.da.silva@tecnico.ulisboa.pt>
2025-06-07 21:05:15 +0100

    feat(tensor): add rank and reshape rules
    Passes all (parser and xml) tests.

--- ast/rank_node.h
+++ ast/rank_node.h
@@ -1,15 +1,16 @@
 #pragma once
 
-#include <cdk/ast/unary_operation_node.h>
+#include <cdk/ast/expression_node.h>
 
 namespace udf {
   /**
    * Class for describing rank_node nodes.
    */
-    class rank_node : public cdk::unary_operation_node {
+    class rank_node : public cdk::expression_node {
+        std::string _identifier;
     public:
-        rank_node(int lineno, cdk::expression_node *arg):
-            cdk::unary_operation_node(lineno, arg) {}
+        rank_node(int lineno, const std::string &identifier) :
+            cdk::expression_node(lineno), _identifier(identifier) {}
 
         void accept(basic_ast_visitor *sp, int level) { sp->do_rank_node(this, level); } 
     };
