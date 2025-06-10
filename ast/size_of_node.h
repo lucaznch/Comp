47a33b90ab4d4a25d0717f3199c29b67a8656c78
Gabriel Silva <gabriel.da.silva@tecnico.ulisboa.pt>
2025-06-10 11:04:34 +0100

    feat(ast): add getter for the sizeof class

--- ast/size_of_node.h
+++ ast/size_of_node.h
@@ -8,8 +8,10 @@ namespace udf {
    */
     class size_of_node : public cdk::unary_operation_node {
     public:
-        size_of_node(int lineno, cdk::expression_node *arg):
+        size_of_node(int lineno, cdk::expression_node *arg = nullptr):
             cdk::unary_operation_node(lineno, arg) {}
+
+        cdk::expression_node *expression() { return argument(); }
 
         void accept(basic_ast_visitor *sp, int level) { sp->do_size_of_node(this, level); } 
     };
